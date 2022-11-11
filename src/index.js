dbca5994a87e25331c412742b261d2f0b7494ce7
Boris-Maslenov <ya.maslenov@yandex.ru>
2022-11-11 14:48:28 +0300

    add swiper slider

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import Swiper, { Navigation } from 'swiper';
 import 'swiper/css';
 import Accordion from './modules/accordion';
 
-
-
-
 //Accordion init
 new Accordion('.accordion', '.accordion__head', '.accordion__content', 222, true).init();
 
