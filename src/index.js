d58cbbfdc8d905d309bbd2f315c6006c36b13e29
Boris-Maslenov <ya.maslenov@yandex.ru>
2022-11-11 14:48:03 +0300

    add swiper slider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import './index.html';
 import './scss/style.scss';
 
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/css';
 import Accordion from './modules/accordion';
+
+
+
 
 //Accordion init
 new Accordion('.accordion', '.accordion__head', '.accordion__content', 222, true).init();
+
+// init Swiper:
+const swiper = new Swiper('.location-slider', {
+modules: [Navigation],
+slidesPerView: 3,
+spaceBetween: 30,
+navigation: {
+    nextEl: '.location-section__slide-next',
+    prevEl: '.location-section__slide-prev',
+    },
+});
